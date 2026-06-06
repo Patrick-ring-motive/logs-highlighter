bdd5f8165b62907d929358a1098e09f9534b9678
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-05 19:03:31 -0500

    Update func.js

--- func.js
+++ func.js
@@ -336,7 +336,7 @@
       }
 
       const targets = [
-        ...document.querySelectorAll('.run-output, .run-output *, .react-code-text, code, [class*="log-viewer"], .yaml-editor, .CodeMirror-lines, pre:not(:has(*)),pre :not(.non-alpha,[class*="highlight"],:has(*)), html:not([data-origin*="jenkins"]) a, time, td'),
+        ...document.querySelectorAll('.run-output, .run-output *, .react-code-text, code, [class*="log-viewer"], .yaml-editor, .CodeMirror-lines, pre:not(:has(*)),pre :not(.non-alpha,[class*="highlight"],:has(*)):last-child, html:not([data-origin*="jenkins"]) a, time, td'),
         document.querySelector("cloudbees-log-viewer-main"),
         document.querySelector(".log-viewer-container"),
       ].filter(Boolean);
